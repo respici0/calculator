1179c80cbec234e4e528fcfb4293de0c9943cf21
respici0 <respicio.robert@gmail.com>
2019-04-11 11:33:06 -0700

    putting in the functions

--- src/App.js
+++ src/App.js
@@ -2,67 +2,173 @@ import React, { Component } from "react";
 import "./App.scss";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialVal: 0,
+      currentVal: 0,
+      sum: 0
+    };
+  }
+
+  onChange = e => {
+    let num = e.target.value;
+    this.setState(
+      {
+        initialVal: num
+      },
+      () => console.log(this.state)
+    );
+  };
+
+  // maybe do a switch statement with the signs?
+  addition = () => {
+    this.state.sum += this.state.initialVal;
+  };
+
+  subtraction = () => {
+    this.state.sum -= this.state.initialVal;
+  };
+
+  multiply = () => {
+    this.state.sum *= this.state.initialVal;
+  };
+
+  division = () => {
+    this.state.sum /= this.state.initialVal;
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="calculator">
           <div className="container-fluid">
-            <div className="col">
+            <div className=" col">
               <div className="row">
-                <div id="display">Display</div>
+                <div className="container-fluid" id="display">
+                  <div className="col">
+                    <div className="row justify-content-end">
+                      {this.state.sum}
+                    </div>
+                    <div className="row justify-content-end">
+                      {this.state.initialVal}
+                    </div>
+                  </div>
+                </div>
               </div>
+
               <div className="row">
                 <button type="button" className="numberButton" id="clear">
                   Clear
                 </button>
                 <button className="numberButton" id="divide">
-                  <i class="fas fa-divide" />
+                  <i className="fas fa-divide" />
                 </button>
               </div>
               <div className="row">
-                <button type="button" className="numberButton" id="seven">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="seven"
+                  value="7"
+                  onClick={this.onChange}
+                >
                   7
                 </button>
-                <button type="button" className="numberButton" id="eight">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="eight"
+                  value="8"
+                  onClick={this.onChange}
+                >
                   8
                 </button>
-                <button type="button" className="numberButton" id="nine">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="nine"
+                  value="9"
+                  onClick={this.onChange}
+                >
                   9
                 </button>
                 <button type="button" className="numberButton" id="multiply">
-                  <i class="fas fa-times" />
+                  <i className="fas fa-times" />
                 </button>
               </div>
               <div className="row">
-                <button type="button" className="numberButton" id="four">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="four"
+                  value="4"
+                  onClick={this.onChange}
+                >
                   4
                 </button>
-                <button type="button" className="numberButton" id="five">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="five"
+                  value="5"
+                  onClick={this.onChange}
+                >
                   5
                 </button>
-                <button type="button" className="numberButton" id="six">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="six"
+                  value="6"
+                  onClick={this.onChange}
+                >
                   6
                 </button>
                 <button type="button" className="numberButton" id="subtract">
-                  <i class="fas fa-minus" />
+                  <i className="fas fa-minus" />
                 </button>
               </div>
               <div className="row">
-                <button type="button" className="numberButton" id="one">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="one"
+                  value="1"
+                  onClick={this.onChange}
+                >
                   1
                 </button>
-                <button type="button" className="numberButton" id="two">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="two"
+                  value="2"
+                  onClick={this.onChange}
+                >
                   2
                 </button>
-                <button type="button" className="numberButton" id="three">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="three"
+                  value="3"
+                  onClick={this.onChange}
+                >
                   3
                 </button>
                 <button type="button" className="numberButton" id="add">
-                  <i class="fas fa-plus" />
+                  <i className="fas fa-plus" />
                 </button>
               </div>
               <div className="row">
-                <button type="button" className="numberButton" id="zero">
+                <button
+                  type="button"
+                  className="numberButton"
+                  id="zero"
+                  value="0"
+                  onClick={this.onChange}
+                >
                   0
                 </button>
                 <button type="button" className="numberButton" id="decimal">
